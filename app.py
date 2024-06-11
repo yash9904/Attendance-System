5025ee71758b5bb1ec098ef5bbe905c6217a6a06
Yash Thakre <86224563+yash9904@users.noreply.github.com>
2024-06-11 22:48:43 +0530

    rename classes

--- app.py
+++ app.py
@@ -1,86 +1,121 @@
-import cv2
 import os
-from flask import Flask,request, render_template
 from datetime import date
 
+import cv2
+from flask import Flask, render_template, request
 
-from pyattendance import attendance, face_detection
+from pyattendance import Attendance, FaceDetection
 
 #### Defining Flask App
 app = Flask(__name__)
 
 today = date.today().strftime("%d-%B-%Y")
 
-atd = attendance()
-face_detector = face_detection()
+atd = Attendance()
+face_detector = FaceDetection()
 
 ################## ROUTING FUNCTIONS #########################
 
+
 #### Our main page
-@app.route('/')
+@app.route("/")
 def home():
-    names, rolls, times, l = atd.extract_attendance()    
-    return render_template('home.html',
-                            names = names, 
-                            rolls = rolls,
-                            times = times,
-                            l = l,
-                            totalreg = atd.totalreg(),
-                            today = today) 
+    names, rolls, times, l = atd.extract_attendance()
+    return render_template(
+        "home.html",
+        names=names,
+        rolls=rolls,
+        times=times,
+        l=l,
+        totalreg=atd.totalreg(),
+        today=today,
+    )
 
 
 #### This function will run when we click on Take Attendance Button
-@app.route('/start',methods=['GET'])
+@app.route("/start", methods=["GET"])
 def start():
-    if 'face_recognition_model.pkl' not in os.listdir('static'):
-        return render_template('home.html',
-                               totalreg = atd.totalreg(),
-                               today = atd.today,
-                               mess = 'There is no trained model in the static folder. Please add a new face to continue.') 
+    if "face_recognition_model.pkl" not in os.listdir("static"):
+        return render_template(
+            "home.html",
+            totalreg=atd.totalreg(),
+            today=atd.today,
+            mess="There is no trained model in the static folder. Please add a new face to continue.",
+        )
 
     cap = cv2.VideoCapture(0)
     ret = True
     face_count = 0
     while ret:
-        ret,frame = cap.read()
+        ret, frame = cap.read()
         if face_detector.is_face_and_eye(frame):
 
             identified_person, frame = face_detector.detect(frame)
-            
+
             atd.add_attendance(identified_person)
-            cv2.putText(frame,f'{identified_person}',(200,35),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 20),2,cv2.LINE_AA)
-            face_count+=1
+            cv2.putText(
+                frame,
+                f"{identified_person}",
+                (200, 35),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 0, 20),
+                2,
+                cv2.LINE_AA,
+            )
+            face_count += 1
 
         elif face_detector.is_eye(frame):
-            identified_person, frame = face_detector.detect(frame, only_eyes = True)
-            
-            atd.add_attendance(identified_person)
-            cv2.putText(frame,f'{identified_person}',(200,35),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 20),2,cv2.LINE_AA)
-            face_count+=1
+            identified_person, frame = face_detector.detect(frame, only_eyes=True)
 
-        cv2.putText(frame,f'Faces: {face_count}/20',(35,35),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 20),2,cv2.LINE_AA)
-        cv2.imshow('Attendance',frame)
-        if (cv2.waitKey(1) & 0xFF == ord('q')) or face_count == 20:
+            atd.add_attendance(identified_person)
+            cv2.putText(
+                frame,
+                f"{identified_person}",
+                (200, 35),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 0, 20),
+                2,
+                cv2.LINE_AA,
+            )
+            face_count += 1
+
+        cv2.putText(
+            frame,
+            f"Faces: {face_count}/20",
+            (35, 35),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 0, 20),
+            2,
+            cv2.LINE_AA,
+        )
+        cv2.imshow("Attendance", frame)
+        if (cv2.waitKey(1) & 0xFF == ord("q")) or face_count == 20:
             break
 
     cap.release()
     cv2.destroyAllWindows()
-    names,rolls,times,l = atd.extract_attendance()    
-    return render_template('home.html',
-                           names = names,
-                           rolls = rolls,
-                           times = times,
-                           l = l,
-                           totalreg = atd.totalreg(),
-                           today = today)
+    names, rolls, times, l = atd.extract_attendance()
+    return render_template(
+        "home.html",
+        names=names,
+        rolls=rolls,
+        times=times,
+        l=l,
+        totalreg=atd.totalreg(),
+        today=today,
+    )
+
 
 #### This function will run when we add a new user
-@app.route('/add',methods=['GET','POST'])
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    newusername = request.form['newusername'].strip()
-    newuserid = request.form['newuserid'].strip()
-    userfacefolder = 'static/faces/'+newusername+'_'+str(newuserid)
-    usereyefolder = 'static/eyes/'+newusername+'_'+str(newuserid)
+    newusername = request.form["newusername"].strip()
+    newuserid = request.form["newuserid"].strip()
+    userfacefolder = "static/faces/" + newusername + "_" + str(newuserid)
+    usereyefolder = "static/eyes/" + newusername + "_" + str(newuserid)
 
     if not os.path.isdir(userfacefolder):
         os.makedirs(userfacefolder)
@@ -93,58 +128,80 @@ def add():
     faces_captured = False
     eyes_captured = False
     while 1:
-        _,frame = cap.read()
+        _, frame = cap.read()
         faces = face_detector.extract_faces(frame)
-        if len(faces) and not(faces_captured):
-            for (x,y,w,h) in faces:
-                cv2.rectangle(frame,(x, y), (x + w, y + h), (255, 0, 20), 2)
-                cv2.putText(frame,f'Face Images Captured: {n_face_caps}/50',(30,30),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 20),2,cv2.LINE_AA)
-                if n_faces % 5==0:
-                    name = newusername+'_'+str(n_face_caps)+'.jpg'
-                    cv2.imwrite(userfacefolder + '/' + name, frame[y: y + h, x: x + w])
-                    n_face_caps+=1
-                n_faces+=1
+        if len(faces) and not (faces_captured):
+            for x, y, w, h in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 20), 2)
+                cv2.putText(
+                    frame,
+                    f"Face Images Captured: {n_face_caps}/50",
+                    (30, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 0, 20),
+                    2,
+                    cv2.LINE_AA,
+                )
+                if n_faces % 5 == 0:
+                    name = newusername + "_" + str(n_face_caps) + ".jpg"
+                    cv2.imwrite(
+                        userfacefolder + "/" + name, frame[y : y + h, x : x + w]
+                    )
+                    n_face_caps += 1
+                n_faces += 1
 
         eyes = face_detector.extract_eyes(frame)
-        
-        if (len(eyes) == 2) and not(eyes_captured):
-            for i, eye in enumerate(eyes):
-                
-                (x,y,w,h) = eye
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 20), 2)
-                cv2.putText(frame,f'Eye Images Captured: {n_eyes//5}/50', (30, 70), cv2.FONT_HERSHEY_SIMPLEX, 1,(255, 0, 20),2,cv2.LINE_AA)
-                if n_eyes%5==0:
-                    name = newusername + '_' + str(n_eyes//5) + '_' + str(i) + '.jpg'
-                    cv2.imwrite(usereyefolder + '/' + name, frame[y: y + h, x: x + w])
-            n_eyes+=1
-        
 
-        if (n_faces == 250):
+        if (len(eyes) == 2) and not (eyes_captured):
+            for i, eye in enumerate(eyes):
+
+                (x, y, w, h) = eye
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 20), 2)
+                cv2.putText(
+                    frame,
+                    f"Eye Images Captured: {n_eyes//5}/50",
+                    (30, 70),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 0, 20),
+                    2,
+                    cv2.LINE_AA,
+                )
+                if n_eyes % 5 == 0:
+                    name = newusername + "_" + str(n_eyes // 5) + "_" + str(i) + ".jpg"
+                    cv2.imwrite(usereyefolder + "/" + name, frame[y : y + h, x : x + w])
+            n_eyes += 1
+
+        if n_faces == 250:
             faces_captured = True
 
-        if (n_eyes == 250):
+        if n_eyes == 250:
             eyes_captured = True
-        
+
         if (n_faces == 250) and (n_eyes == 250):
             break
 
-        cv2.imshow('Adding new User',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow("Adding new User", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
     cap.release()
     cv2.destroyAllWindows()
-    print('Training Model')
+    print("Training Model")
     face_detector.train_model()
-    names,rolls,times,l = atd.extract_attendance()    
-    return render_template('home.html',
-                           names = names,
-                           rolls = rolls,
-                           times = times,
-                           l = l,
-                           totalreg = atd.totalreg(),
-                           today = today) 
+    names, rolls, times, l = atd.extract_attendance()
+    return render_template(
+        "home.html",
+        names=names,
+        rolls=rolls,
+        times=times,
+        l=l,
+        totalreg=atd.totalreg(),
+        today=today,
+    )
+
 
 #### Our main function which runs the Flask App
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- pyattendance.py
+++ pyattendance.py
@@ -1,53 +1,58 @@
-import cv2
 import os
-from datetime import date
-from datetime import datetime
-import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
-import pandas as pd
-import joblib
+from datetime import date, datetime
 
-class attendance:
+import cv2
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
+
+class Attendance:
     def __init__(self):
         self.datetoday = date.today().strftime("%m_%d_%y")
-        
-        if not os.path.isdir('Attendance'):
-            os.makedirs('Attendance')
-        if not os.path.isdir('static/faces'):
-            os.makedirs('static/faces')
-        if not os.path.isdir('static/eyes'):
-            os.makedirs('static/eyes')
-        if f'Attendance-{self.datetoday}.csv' not in os.listdir('Attendance'):
-            with open(f'Attendance/Attendance-{self.datetoday}.csv','w') as f:
-                f.write('Name,Roll,Time')
+
+        if not os.path.isdir("Attendance"):
+            os.makedirs("Attendance")
+        if not os.path.isdir("static/faces"):
+            os.makedirs("static/faces")
+        if not os.path.isdir("static/eyes"):
+            os.makedirs("static/eyes")
+        if f"Attendance-{self.datetoday}.csv" not in os.listdir("Attendance"):
+            with open(f"Attendance/Attendance-{self.datetoday}.csv", "w") as f:
+                f.write("Name,Roll,Time")
 
     def extract_attendance(self):
-        df = pd.read_csv(f'Attendance/Attendance-{self.datetoday}.csv')
-        names = df['Name']
-        rolls = df['Roll']
-        times = df['Time']
+        df = pd.read_csv(f"Attendance/Attendance-{self.datetoday}.csv")
+        names = df["Name"]
+        rolls = df["Roll"]
+        times = df["Time"]
         l = len(df)
-        return names,rolls,times,l
+        return names, rolls, times, l
 
     #### Add Attendance of a specific user
     def add_attendance(self, name):
-        username = name.split('_')[0]
-        userid = name.split('_')[1]
+        username = name.split("_")[0]
+        userid = name.split("_")[1]
         current_time = datetime.now().strftime("%H:%M:%S")
-        
-        df = pd.read_csv(f'Attendance/Attendance-{self.datetoday}.csv')
-        if int(userid) not in list(df['Roll']):
-            with open(f'Attendance/Attendance-{self.datetoday}.csv','a') as f:
-                f.write(f'\n{username},{userid},{current_time}')
+
+        df = pd.read_csv(f"Attendance/Attendance-{self.datetoday}.csv")
+        if int(userid) not in list(df["Roll"]):
+            with open(f"Attendance/Attendance-{self.datetoday}.csv", "a") as f:
+                f.write(f"\n{username},{userid},{current_time}")
 
     def totalreg(self):
-        return len(os.listdir('static/faces'))
+        return len(os.listdir("static/faces"))
 
 
-class face_detection:
+class FaceDetection:
     def __init__(self):
-        self.face_detector = cv2.CascadeClassifier('static/haarcascade_frontalface_default.xml')
-        self.eye_detector = cv2.CascadeClassifier('static/haarcascade_eye_tree_eyeglasses.xml')
+        self.face_detector = cv2.CascadeClassifier(
+            "static/haarcascade_frontalface_default.xml"
+        )
+        self.eye_detector = cv2.CascadeClassifier(
+            "static/haarcascade_eye_tree_eyeglasses.xml"
+        )
 
     def create_eye_features(self, *args):
         features = tuple(map(lambda img: cv2.resize(img, (10, 10)).ravel(), args))
@@ -58,9 +63,9 @@ class face_detection:
         face = cv2.resize(face, (50, 50))
         eye_features = self.create_eye_features(eye_1, eye_2)
         features = np.hstack((face.ravel(), eye_features))
-        
+
         return features
-    
+
     #### extract the face from an image
     def extract_eyes(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -75,54 +80,53 @@ class face_detection:
 
     #### Identify face using ML model
     def identify_face(self, facearray):
-        model = joblib.load('static/face_recognition_model.pkl')
+        model = joblib.load("static/face_recognition_model.pkl")
         return model.predict(facearray)
-    
+
     def identify_eye(self, eyearray):
-        model = joblib.load('static/eye_recognition_model.pkl')
+        model = joblib.load("static/eye_recognition_model.pkl")
         return model.predict(eyearray)
 
     #### A function which trains the model on all the faces available in faces folder
     def train_model(self):
         features = []
         labels = []
-        userlist = os.listdir('static/faces')
+        userlist = os.listdir("static/faces")
 
         for user in userlist:
-            for imgname in os.listdir(f'static/faces/{user}'):
-                img = cv2.imread(f'static/faces/{user}/{imgname}')
+            for imgname in os.listdir(f"static/faces/{user}"):
+                img = cv2.imread(f"static/faces/{user}/{imgname}")
                 face = cv2.resize(img, (50, 50))
 
-                eye_1 = cv2.imread(f'static/eyes/{user}/{imgname[: -4]}_0.jpg')
-                eye_2 = cv2.imread(f'static/eyes/{user}/{imgname[: -4]}_1.jpg')
-                
+                eye_1 = cv2.imread(f"static/eyes/{user}/{imgname[: -4]}_0.jpg")
+                eye_2 = cv2.imread(f"static/eyes/{user}/{imgname[: -4]}_1.jpg")
+
                 labels.append(user)
                 features.append(self.create_features(face, eye_1, eye_2))
 
         features = np.array(features)
-        knn = KNeighborsClassifier(n_neighbors = 5)
+        knn = KNeighborsClassifier(n_neighbors=5)
         knn.fit(features, labels)
-        joblib.dump(knn, 'static/face_recognition_model.pkl')
+        joblib.dump(knn, "static/face_recognition_model.pkl")
 
-        knn = KNeighborsClassifier(n_neighbors = 5)
+        knn = KNeighborsClassifier(n_neighbors=5)
         knn.fit(features[:, -600:], labels)
-        joblib.dump(knn, 'static/eye_recognition_model.pkl')
+        joblib.dump(knn, "static/eye_recognition_model.pkl")
 
-    
-    def detect(self, frame, only_eyes = False):
-        if not(only_eyes):
-            (x,y,w,h) = self.extract_faces(frame)[0]
-            cv2.rectangle(frame,(x, y), (x+w, y+h), (255, 0, 20), 2)
-            face = frame[y:y+h,x:x+w]
+    def detect(self, frame, only_eyes=False):
+        if not (only_eyes):
+            (x, y, w, h) = self.extract_faces(frame)[0]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 20), 2)
+            face = frame[y : y + h, x : x + w]
 
         eye_1_coords, eye_2_coords = self.extract_eyes(frame)
-        (x,y,w,h) = eye_1_coords
-        cv2.rectangle(frame,(x, y), (x+w, y+h), (255, 0, 20), 2)
-        eye_1 = frame[y:y+h,x:x+w]
+        (x, y, w, h) = eye_1_coords
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 20), 2)
+        eye_1 = frame[y : y + h, x : x + w]
 
-        (x,y,w,h) = eye_2_coords
-        cv2.rectangle(frame,(x, y), (x+w, y+h), (255, 0, 20), 2)
-        eye_2 = frame[y:y+h,x:x+w]
+        (x, y, w, h) = eye_2_coords
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 20), 2)
+        eye_2 = frame[y : y + h, x : x + w]
 
         if only_eyes:
             features = self.create_eye_features(eye_1, eye_2)[np.newaxis, ...]
@@ -133,9 +137,13 @@ class face_detection:
 
         return identified_person, frame
 
-
     def is_face_and_eye(self, frame):
-        return True if np.any(self.extract_faces(frame)) and (len(self.extract_eyes(frame)) == 2) else False
-    
+        return (
+            True
+            if np.any(self.extract_faces(frame))
+            and (len(self.extract_eyes(frame)) == 2)
+            else False
+        )
+
     def is_eye(self, frame):
         return True if (len(self.extract_eyes(frame)) == 2) else False
